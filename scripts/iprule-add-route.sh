b8bc3b0c57f5b9f5d166c4f67ab93ea4f923cbb9
Michael Davidson <mr.m.davidson@gmail.com>
2017-04-18 22:48:40 +1000

    Change to using route instead of ip route

--- scripts/iprule-add-route.sh
+++ scripts/iprule-add-route.sh
@@ -5,7 +5,9 @@ DEVNAME=$2
 GATEWAY=$3
 
 if [ -z "$GATEWAY" ]; then
-    ip route add $IPADDR dev $DEVNAME table $DEVNAME
+    route add -host $IPADDR dev $DEVNAME
+    #ip route add $IPADDR dev $DEVNAME table $DEVNAME
 else
-    ip route add $IPADDR via $GATEWAY dev $DEVNAME table $DEVNAME
+    #ip route add $IPADDR via $GATEWAY dev $DEVNAME table $DEVNAME
+    route add -host $IPADDR gw $GATEWAY dev $DEVNAME
 fi
